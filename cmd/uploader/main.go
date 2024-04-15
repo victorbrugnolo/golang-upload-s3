5f120d8da3adee5d4dc9d8def343532c74433825
victorbrugnolo <brugnolovictor@gmail.com>
2024-04-14 22:03:20 -0300

    create upload function

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -33,5 +36,39 @@ func init() {
 }
 
 func main() {
+	dir, err := os.Open("./tmp")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer dir.Close()
+}
+
+func uploadFile(filename string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s \n", completeFileName, s3Bucket)
+
+	f, err := os.Open(completeFileName)
+
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", completeFileName)
+		return
+	}
+
+	defer f.Close()
+
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+
+	if err != nil {
+		fmt.Printf("Error uploading file %s\n", completeFileName)
+		return
+	}
+
+	fmt.Printf("File %s uploaded successfully \n", completeFileName)
 
 }
