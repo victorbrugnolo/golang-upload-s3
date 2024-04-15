cb1f73c16d00fc3d18f45d80724d5eb2d55505a5
victorbrugnolo <brugnolovictor@gmail.com>
2024-04-14 21:53:26 -0300

    configure aws session

---
+++ cmd/uploader/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"example",
+				"exaple",
+				"",
+			),
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client = s3.New(sess)
+	s3Bucket = "golang-upload-tmp-files"
+}
+
+func main() {
+
+}
